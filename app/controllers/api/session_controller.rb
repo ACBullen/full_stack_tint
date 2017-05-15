289e5e95fc54ffb658d8a6cc12d8683d002d01a8
Alexander Bullen <acbullen1990@gmail.coml>
2017-05-15 14:41:24 -0700

    Correct session routing, add user profile pic to user show, set up session create method

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,19 @@
 class Api::SessionController < ApplicationController
+  def create
+    @user.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      log_in(@user)
+      render json: { username: @user.username, id: @user.id }
+    else
+      render json: "Invalid username/password combination", status: 401
+    end
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :user, only: [:create]
-    resources :session, only: [:create, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 end
