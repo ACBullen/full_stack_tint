29cbd64d7d6629c8c75c678af6636fba43a4c735
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-15 15:22:52 -0700

    Add profile pic to info sent from user show page, set up script tag for current user bootstrapping, import apiutils to window for testing purposes

--- frontend/tint.jsx
+++ frontend/tint.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// files brought in for testing purposes only. Remove when unneeded
+import * as APIUtilS from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.APIUtilS = APIUtilS;
   ReactDOM.render(<h1>Tint Test</h1>, root)
 });
