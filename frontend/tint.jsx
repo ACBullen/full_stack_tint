298ac442b06e23daec05c63788c19325bea05975
Alexander Bullen <acbullen1990@gmail.com>
2017-05-15 16:38:22 -0700

    complete current user bootstrapping, add store to window to test

--- frontend/tint.jsx
+++ frontend/tint.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 // files brought in for testing purposes only. Remove when unneeded
 import * as APIUtilS from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  let store;
+  if (window.currentUser) {
+    store = configureStore({currentUser: window.currentUser});
+  } else {
+    store = configureStore();
+  }
+  window.store = store;
   window.APIUtilS = APIUtilS;
   ReactDOM.render(<h1>Tint Test</h1>, root)
 });
