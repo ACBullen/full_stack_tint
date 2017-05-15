3f7ca2edbbc11a6a35b5c2c47a09bc09b64b447c
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-15 14:30:21 -0700

    Format user controller create method to render json show view, create user show view and user partial

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::UsersController < ApplicationController
-  
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user
+      render :show
     else
-      render json: @user.errors.full_messages
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
