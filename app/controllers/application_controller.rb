86ef8add476159d897c74f3e68cd9f67cb9e8d6b
Alexander Bullen <acbullen1990@gmail.com>
2017-05-15 14:56:21 -0700

    Correct sessions controller name, add test dummy display for current user username, correct users route name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   helper_method :current_user
+
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def log_in(user)
+    session[:session_token] = user.session_token
   end
 end
