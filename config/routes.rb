2a2f44d813aefd917c3f7d1f72011e080ac77064
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-15 14:56:21 -0700

    Correct sessions controller name, add test dummy display for current user username, correct users route name

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "static_pages#index"
 
   namespace :api, defaults: { format: :json } do
-    resources :user, only: [:create]
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
 end
