c67bcbc76090fd59f8c7a824e9e356f87438e51f
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-15 13:50:11 -0700

    Add annotate, bcrypt. Annotate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  profile_pic     :string           default("http://www.drodd.com/images15/letter-t37.jpg"), not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, :session_token, :password_digest, presence: true
   validates :username, :session_token, uniqueness: true
+
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
 
