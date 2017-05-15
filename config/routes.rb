327694faa8f6da7bacd1321a13d67e705bf76f78
Alexander Bullen <acbullen1990@gmail.com>
2017-05-15 14:11:14 -0700

    Correct static_pages route to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :static_pages, only: [:index]
+  root to: "static_pages#index"
 end
