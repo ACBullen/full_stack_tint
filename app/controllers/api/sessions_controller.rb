a4f944bacc06a9a1a0470075796baf5168ca3dea
Alexander Bullen <acbullen1990@gmail.com>
2017-05-15 15:22:52 -0700

    Add profile pic to info sent from user show page, set up script tag for current user bootstrapping, import apiutils to window for testing purposes

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,7 +8,7 @@ class Api::SessionsController < ApplicationController
     if @user
       log_in(@user)
       p current_user
-      render json: { username: @user.username, id: @user.id }
+      render json: { username: @user.username, id: @user.id, profile_pic: @user.profile_pic }
     else
       render json: "Invalid username/password combination", status: 401
     end
