fa47503b448d3c3308e3092ce407e57d7d349022
Alexander Bullen <acbullen1990@gmail.coml>
2017-05-15 13:50:11 -0700

    Add annotate, bcrypt. Annotate user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  profile_pic     :string           default("http://www.drodd.com/images15/letter-t37.jpg"), not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
