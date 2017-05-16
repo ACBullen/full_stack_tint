2bed85c3e0a8e7c0865ac320234eccbf36cb9b4f
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-16 09:57:52 -0700

    Implement protected auth routes and bugfix

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import SessionFormContainer from './session_form_container';
 import NavContainer from './nav_container';
+import { AuthRoute } from '../util/auth_util';
 //Separate h1 into a nav bar component with the related info in later
 //refactoring.
 
@@ -11,8 +12,8 @@ const Main = ({store}) => (
       <NavContainer />
     </header>
     <div id="MainBody">
-      <Route exact path= "/login" component={SessionFormContainer} />
-      <Route path= "/signup" component={SessionFormContainer} />
+      <AuthRoute exact path= "/login" component={SessionFormContainer} />
+      <AuthRoute exact path= "/signup" component={SessionFormContainer} />
     </div>
   </div>
 )
