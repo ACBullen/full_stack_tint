f8f36eaf944fb5902e124f4a667b66ecb8571dce
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-16 10:50:54 -0700

    Switch db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170515190427) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username",                                                                 null: false
     t.string   "password_digest",                                                          null: false
@@ -19,8 +22,8 @@ ActiveRecord::Schema.define(version: 20170515190427) do
     t.string   "session_token",                                                            null: false
     t.datetime "created_at",                                                               null: false
     t.datetime "updated_at",                                                               null: false
-    t.index ["session_token"], name: "index_users_on_session_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
 end
