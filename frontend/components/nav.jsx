a679102ddff66ce8d7794119d03d04c780c1e416
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-16 08:23:03 -0700

    Create nav container and dummy nav for nav bar

---
+++ frontend/components/nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <h1>I am a nav bar</h1>
+    )
+  }
+}
+
+export default Nav;
