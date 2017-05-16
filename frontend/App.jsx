1f609f00335987b267f827a428c805a0729be1db
Alexander Bullen <acbullen1990@gmail.coml>
2017-05-15 17:12:13 -0700

    Create initial component chain to render the auth form

---
+++ frontend/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import Main from './components/main';
+
+const App = ({store}) => {
+  console.log("App rendering");
+return(
+  <div>
+    <Provider store={store}>
+      <HashRouter>
+        <Main />
+      </HashRouter>
+    </Provider>
+  </div>
+)};
+
+export default App;
