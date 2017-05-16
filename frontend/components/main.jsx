e1e90867848e6a2c93394b4a3f20dccac614d2c9
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-16 11:40:51 -0700

    Add login link when logged out. Redirect logged in users away from login/signup pages

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -6,16 +6,30 @@ import { AuthRoute } from '../util/auth_util';
 //Separate h1 into a nav bar component with the related info in later
 //refactoring.
 
-const Main = ({store}) => (
-  <div>
-    <header>
-      <NavContainer />
-    </header>
-    <div id="MainBody">
-      <AuthRoute exact path= "/login" component={SessionFormContainer} />
-      <AuthRoute exact path= "/signup" component={SessionFormContainer} />
-    </div>
-  </div>
-)
+class Main extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentWillReceiveProps(newProps){
+    console.log(newProps);
+    this.forceUpdate()
+  }
+
+  render () {
+    return(
+      <div>
+        <header>
+          <NavContainer />
+        </header>
+        <div id="MainBody">
+          <AuthRoute exact path= "/login" component={SessionFormContainer} />
+          <AuthRoute exact path= "/signup" component={SessionFormContainer} />
+        </div>
+      </div>
+    )
+  }
+}
+
 
 export default Main;
