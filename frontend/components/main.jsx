e68fedc6c79a0b9c327b47fb0ebbc2c262dadafe
Alexander Bullen <acbullen1990@gmail.com>
2017-05-16 09:02:17 -0700

    Add click handling to demo sign-in. Add logout button and username display to navbar. Changed log in method to not expose password

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SessionFormContainer from './session_form_container';
+import NavContainer from './nav_container';
 //Separate h1 into a nav bar component with the related info in later
 //refactoring.
 
 const Main = ({store}) => (
   <div>
     <header>
-      <h1>T.I.N.T.</h1>
+      <NavContainer />
     </header>
-    
+
     <Route path= "/" component={SessionFormContainer} />
   </div>
 )
