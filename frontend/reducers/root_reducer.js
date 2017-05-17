5ff7544f8d97db8fc2c54e3c79c5c7aea533bd3d
Alexander Bullen <acbullen1990@gmail.com>
2017-05-16 22:28:12 -0700

    Create user actions and reducer. Bugfix errors reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
+import UsersReducer from './users_reducer';
 import PostReducer from './post_reducer';
 
 
 const RootReducer = combineReducers({
   currentUser: SessionReducer,
   errors: ErrorsReducer,
+  users: UsersReducer,
   posts: PostReducer
 });
 
