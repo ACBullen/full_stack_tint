938ef93b3270beae6809e9712e38feefee86d279
Alexander Bullen <acbullen1990@gmail.com>
2017-05-16 17:26:16 -0700

    Finish post index and create methods. Construct post views

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,31 @@
 class Api::PostsController < ApplicationController
 
   def index
-    @posts = Post.all
-    
+    @posts = Post.all.order(:created_at).include(:user)
+    render :index
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: :post
+    else
+      render json: @post.errors.full_messages
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_type,
+                                 :title,
+                                 :body,
+                                 :user_id,
+                                 :link_url,
+                                 :media_link,
+                                 :user_id
+                                )
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
-    post :posts, only: [:create, :index]
+    resources :posts, only: [:create, :index]
   end
 end
