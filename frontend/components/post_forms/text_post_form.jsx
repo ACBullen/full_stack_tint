6785ebc7b89fed6ac322d2af75dcf213cd4a95b5
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-05-17 14:41:08 -0700

    Add icons to select additional media to input. Add click handlers to expose correct input.

--- frontend/components/post_forms/text_post_form.jsx
+++ frontend/components/post_forms/text_post_form.jsx
@@ -5,14 +5,14 @@ class TextPostForm extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      post:{
-        title: '',
-        body: '',
-        link_url: undefined,
-        media_link: undefined,
-        post_type: 'text',
-        user_id: this.props.userId
-      }
+
+      title: '',
+      body: '',
+      link_url: undefined,
+      media_link: undefined,
+      post_type: 'text',
+      user_id: this.props.userId
+
     };
 
     this.handleTitleInput = this.handleTitleInput.bind(this);
@@ -38,28 +38,55 @@ class TextPostForm extends React.Component {
     } else {
       this.props.createPost(this.state.post).then(()=> (
         this.setState({
-          post:{
-            title: '',
-            body: '',
-            link_url: undefined,
-            media_link: undefined,
-            post_type: 'text',
-            user_id: this.props.userId
-          }
+          title: '',
+          body: '',
+          link_url: undefined,
+          media_link: undefined,
+          post_type: 'text',
+          user_id: this.props.userId
         })
       ));
     }
   }
 
   showLinkInput(e){
-    let linkInput = document.getElementById("additionalLink");
-
+    let linkInput= document.getElementById("additionalLink");
+    let imageInput= document.getElementById("additionalImage");
+    let videoInput= document.getElementById("additionalVid");
     if (linkInput.style.display === ""){
       linkInput.style.display = 'flex';
+      imageInput.style.display = '';
+      videoInput.style.display = '';
     } else {
       linkInput.style.display = '';
     }
+    this.setState({})
+  }
 
+  showImageInput(e){
+    let linkInput= document.getElementById("additionalLink");
+    let imageInput= document.getElementById("additionalImage");
+    let videoInput= document.getElementById("additionalVid");
+    if (imageInput.style.display === ""){
+      linkInput.style.display = '';
+      imageInput.style.display = 'flex';
+      videoInput.style.display = '';
+    } else {
+      imageInput.style.display = '';
+    }
+  }
+
+  showVideoInput(e){
+    let linkInput= document.getElementById("additionalLink");
+    let imageInput= document.getElementById("additionalImage");
+    let videoInput= document.getElementById("additionalVid");
+    if (videoInput.style.display === ""){
+      linkInput.style.display = '';
+      imageInput.style.display = '';
+      videoInput.style.display = 'flex';
+    } else {
+      videoInput.style.display = '';
+    }
   }
 
   render() {
@@ -68,9 +95,17 @@ class TextPostForm extends React.Component {
         <lable>Title:
         <input onChange={this.handleTitleInput} type='text' placeholder="Your title here!" value={this.state.title}/>
         <i className="fa fa-link" aria-hidden="true" onClick={this.showLinkInput}></i>
+        <i className="fa fa-video-camera" aria-hidden="true"onClick={this.showVideoInput}></i>
+        <i className="fa fa-camera" aria-hidden="true"onClick={this.showImageInput}></i>
         </lable>
         <lable id="additionalLink"><i className="fa fa-link" aria-hidden="true"></i>
         <input type="text" placeholder="Input link here!" />
+        </lable>
+        <lable id="additionalImage"><i className="fa fa-camera" aria-hidden="true"></i>
+        <input type="text" placeholder="Input image link here!" />
+        </lable>
+        <lable id="additionalVid"><i className="fa fa-video-camera" aria-hidden="true"></i>
+        <input type="text" placeholder="Input video link here!" />
         </lable>
         <lable>Body:
           <textarea onChange={this.handleBodyInput} placeholder="Your text here!" value={this.state.body}></textarea>
