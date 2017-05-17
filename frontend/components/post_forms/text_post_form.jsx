9dc2a5d7d5c953ed6040a179a20766d7602cc29d
Alexander Bullen <acbullen1990@gmail.coml>
2017-05-17 14:57:34 -0700

    Allow both a media link and link url to submitted. Set media link initial value to empty string rather than undefined.

--- frontend/components/post_forms/text_post_form.jsx
+++ frontend/components/post_forms/text_post_form.jsx
@@ -8,14 +8,15 @@ class TextPostForm extends React.Component {
 
       title: '',
       body: '',
-      link_url: undefined,
-      media_link: undefined,
+      link_url: '',
+      media_link: '',
       post_type: 'text',
       user_id: this.props.userId
 
     };
 
     this.handleTitleInput = this.handleTitleInput.bind(this);
+    this.handleMediaInput = this.handleMediaInput.bind(this);
     this.handleBodyInput = this.handleBodyInput.bind(this);
     this.showLinkInput = this.showLinkInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -31,6 +32,11 @@ class TextPostForm extends React.Component {
     this.setState({body: e.currentTarget.value});
   }
 
+  handleMediaInput(e){
+    e.preventDefault();
+    this.setState({media_link: e.currentTarget.value})
+  }
+
   handleSubmit (e){
     e.preventDefault();
     if (this.state.title.length < 3) {
@@ -40,8 +46,8 @@ class TextPostForm extends React.Component {
         this.setState({
           title: '',
           body: '',
-          link_url: undefined,
-          media_link: undefined,
+          link_url: '',
+          media_link: '',
           post_type: 'text',
           user_id: this.props.userId
         })
@@ -51,42 +57,46 @@ class TextPostForm extends React.Component {
 
   showLinkInput(e){
     let linkInput= document.getElementById("additionalLink");
-    let imageInput= document.getElementById("additionalImage");
-    let videoInput= document.getElementById("additionalVid");
+
     if (linkInput.style.display === ""){
       linkInput.style.display = 'flex';
-      imageInput.style.display = '';
-      videoInput.style.display = '';
+
     } else {
       linkInput.style.display = '';
+      this.setState({link_url: ''})
     }
-    this.setState({})
   }
 
   showImageInput(e){
-    let linkInput= document.getElementById("additionalLink");
+
     let imageInput= document.getElementById("additionalImage");
     let videoInput= document.getElementById("additionalVid");
+
     if (imageInput.style.display === ""){
-      linkInput.style.display = '';
+
       imageInput.style.display = 'flex';
       videoInput.style.display = '';
+
     } else {
       imageInput.style.display = '';
     }
+    this.setState({media_link: ''});
   }
 
   showVideoInput(e){
-    let linkInput= document.getElementById("additionalLink");
+
     let imageInput= document.getElementById("additionalImage");
     let videoInput= document.getElementById("additionalVid");
+
     if (videoInput.style.display === ""){
-      linkInput.style.display = '';
+  
       imageInput.style.display = '';
       videoInput.style.display = 'flex';
+
     } else {
       videoInput.style.display = '';
     }
+    this.setState({media_link: ''});
   }
 
   render() {
@@ -94,22 +104,27 @@ class TextPostForm extends React.Component {
       <div id="TextPostForm" className='baseLozenge'>
         <lable>Title:
         <input onChange={this.handleTitleInput} type='text' placeholder="Your title here!" value={this.state.title}/>
-        <i className="fa fa-link" aria-hidden="true" onClick={this.showLinkInput}></i>
-        <i className="fa fa-video-camera" aria-hidden="true"onClick={this.showVideoInput}></i>
-        <i className="fa fa-camera" aria-hidden="true"onClick={this.showImageInput}></i>
+        <i className="fa fa-link" aria-hidden="true" onClick={this.showLinkInput.bind(this)}></i>
+        <i className="fa fa-video-camera" aria-hidden="true"onClick={this.showVideoInput.bind(this)}></i>
+        <i className="fa fa-camera" aria-hidden="true"onClick={this.showImageInput.bind(this)}></i>
         </lable>
+
         <lable id="additionalLink"><i className="fa fa-link" aria-hidden="true"></i>
-        <input type="text" placeholder="Input link here!" />
+        <input type="text" value={this.state.media_link} placeholder="Input link here!" onChange={this.handleMediaInput.bind(this)}/>
         </lable>
+
         <lable id="additionalImage"><i className="fa fa-camera" aria-hidden="true"></i>
-        <input type="text" placeholder="Input image link here!" />
+        <input type="text" value={this.state.media_link} placeholder="Input image link here!" onChange={this.handleMediaInput.bind(this)}/>
         </lable>
+
         <lable id="additionalVid"><i className="fa fa-video-camera" aria-hidden="true"></i>
-        <input type="text" placeholder="Input video link here!" />
+        <input type="text" value={this.state.media_link} placeholder="Input video link here!" onChange={this.handleMediaInput.bind(this)}/>
         </lable>
+
         <lable>Body:
           <textarea onChange={this.handleBodyInput} placeholder="Your text here!" value={this.state.body}></textarea>
         </lable>
+
         <button onClick={this.handleSubmit} type="button">Post</button>
       </div>
     )
