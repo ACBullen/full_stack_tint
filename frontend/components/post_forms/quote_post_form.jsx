a954b9d2a59b2f0552d942f2fd210e6d63a386d0
Alexander Bullen <acbullen1990@gmail.com>
2017-05-17 17:54:52 -0700

    Block out the quote form and submission handler. Add base styling

--- frontend/components/post_forms/quote_post_form.jsx
+++ frontend/components/post_forms/quote_post_form.jsx
@@ -12,12 +12,31 @@ class QuotePostForm extends React.Component {
       post_type: 'text',
       user_id: this.props.userId
     };
+    this.handleQuoteInput = this.handleQuoteInput.bind(this);
+    this.handleAuthorInput = this.handleAuthorInput.bind(this);
+  }
+
+  handleQuoteInput(e){
+    e.preventDefault();
+    this.setState({body: e.target.value});
+  }
+
+  handleAuthorInput(e){
+    e.preventDefault();
+    this.setState({title: e.target.value});
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log("click!");
   }
 
   render() {
     return(
-      <div className="baseLozenge" >
-        <h1>Qu</h1>
+      <div id="QuotePostForm" className="baseLozenge" >
+        <textarea onChange={this.handleQuoteInput} placeholder="Give us a quote..." value={this.state.body}></textarea>
+        <input onChange={this.handleAuthorInput} placeholder="Quote Author" value={this.state.title}/>
+        <button onClick={this.handleSubmit}>Post Quote</button>
       </div>
     )
   }
