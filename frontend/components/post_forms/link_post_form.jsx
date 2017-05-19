70aa7f80e33780785ff4e0d79d9d51e89b6677a5
Alexander Bullen <acbullen1990@gmail.com>
2017-05-19 10:36:25 -0700

    Clean up post forms

--- frontend/components/post_forms/link_post_form.jsx
+++ frontend/components/post_forms/link_post_form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostFormContainer from './post_form_container'
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 
 class LinkPostForm extends React.Component {
@@ -15,18 +15,38 @@ class LinkPostForm extends React.Component {
       media_type: '',
       user_id: this.props.userId
     };
+    this.handleLinkInput = this.handleLinkInput.bind(this);
+    this.handleDescInput = this.handleDescInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount(){
     this.props.requestCloudinaryKeys();
   }
 
+  handleLinkInput (e){
+    this.setState({link_url: e.target.value})
+  }
+
+  handleDescInput(e) {
+    this.setState({body: e.target.value})
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    if(this.link_url.length < 5){
+      alert('please input a valid link');
+    } else {
+      this.props.createPost(this.state).then(()=> this.props.history.push('/'));
+    }
+  }
+
+
   render(){
     return (
       <div id="LinkPostForm" className="baseLozenge">
         <form id="LinkPostInputs">
-        <input type="text" placeholder="Link content here" />
-        <input type="text" placeholder="Give the link a description" />
+        <input onChange={this.handleLinkInput} value={this.state.link_url} type="text" placeholder="Link content here" />
+        <input onChange={this.handleDescInput} value={this.state.body} type="text" placeholder="Give the link a description" />
         <div id="controlButtons">
           <Link to='/'><button type="button">Close</button></Link>
           <button onClick={this.handleSubmit} type="button">Post</button>
@@ -37,4 +57,4 @@ class LinkPostForm extends React.Component {
   }
 }
 
-export default PostFormContainer(LinkPostForm);
+export default withRouter(PostFormContainer(LinkPostForm));
