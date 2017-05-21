d18f343c565cff52cfc2865f18c07f414280f528
Alexander Bullen <acbullen1990@gmail.com>
2017-05-21 14:17:32 -0700

    Remove testing variables from window namespace

--- frontend/tint.jsx
+++ frontend/tint.jsx
@@ -5,11 +5,11 @@ import configureStore from './store/store';
 import App from './App'
 
 // files brought in for testing purposes only. Remove when unneeded
-// import * as APIUtilS from './util/session_api_util';
-import * as APIUtilP from './util/post_api_util';
-import * as FollowActions from './actions/user_actions';
-import { getPosts, getMyPosts } from './actions/post_actions';
-// import { requestCloudinaryKeys } from './actions/api_key_actions';
+// // import * as APIUtilS from './util/session_api_util';
+// import * as APIUtilP from './util/post_api_util';
+// import * as FollowActions from './actions/user_actions';
+// import { getPosts, getMyPosts } from './actions/post_actions';
+// // import { requestCloudinaryKeys } from './actions/api_key_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,9 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.APIUtilP = APIUtilP;
-  window.getMyPosts = getMyPosts;
-  window.store = store;
-  window.FollowActions = FollowActions;
+
   ReactDOM.render(<App store={store} />, root)
 });
