94dbfc7b575e7afb5259bb045bc57df4ed8d7554
Alexander Bullen <acbullen1990@gmail.com>
2017-05-22 09:59:43 -0700

    Set up likes migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,12 @@ class User < ApplicationRecord
 
   has_many :posts
 
+  has_many :likes
+
+  has_many :liked_posts,
+    through: :likes,
+    source: :post
+
   has_many :follows,
     primary_key: :id,
     foreign_key: :follower_id,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520225716) do
+ActiveRecord::Schema.define(version: 20170522165018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20170520225716) do
     t.integer  "followee_id", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "post_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true, using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
