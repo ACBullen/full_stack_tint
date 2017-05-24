e7b569f7aecaae74b6852108b63b3df282004f8a
Alexander Bullen <acbullen1990@gmail.com>
2017-05-24 09:52:02 -0700

    Style and insert testing post footer. Correct initial state of post on updating post forms

--- frontend/components/post_forms/video_post_form.jsx
+++ frontend/components/post_forms/video_post_form.jsx
@@ -7,7 +7,10 @@ class VideoPostForm extends React.Component {
   constructor(props){
     super(props);
     if(this.props.post) {
-      this.state = this.props.post;
+      let post = Object.assign({}, this.props.post);
+      post.created_at = undefined;
+      post.id = undefined;
+      this.state = post;
     } else {
       this.state = {
         title: '',
