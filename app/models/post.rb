6cfb6f127ae0ef0f1ab6b124770b7a8ff99d548f
Alexander Bullen <acbullen1990@gmail.com>
2017-05-25 16:51:47 -0700

    Create react component for reblog post content. Adjust post show jbuilder to include rb_post_id. Add reblog post to post base

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@
 class Post < ApplicationRecord
   validates :user_id, :post_type, presence: true
 
-  validates :post_type, inclusion: { in: ["text", "quote", "audio", 'video', "image", "link"] }, allow_nil: true
+  validates :post_type, inclusion: { in: ["text", "quote", "audio", 'video', "image", "link", "reblog"] }, allow_nil: true
   validates :media_type, inclusion: { in: ["audio", "video", "image", "none"] }, allow_nil: true
 
   belongs_to :original_author,
