64653c0f0ba9e5f47e4dc75db095148affde5e1c
Alexander Bullen <acbullen1990@gmail.com>
2017-05-25 13:31:58 -0700

    Extract header-footer logic from individual post types to postbases. Style new content base divs in post bases. Work on reblog post form.

--- frontend/components/post_bases/audio_post.jsx
+++ frontend/components/post_bases/audio_post.jsx
@@ -3,18 +3,17 @@ import PostHeader from './post_header';
 import PostFooter from './post_footer';
 
 
-const AudioPost = ({post, user, deletePost, currentUser, fi, orAuth}) => {
+const AudioPost = ({post}) => {
 
   let source;
   post.media_link ? source = post.media_link : source = post.link_url;
   return (
-    <div id="AudioPost" className={ fi === "true" ? "baseLozenge feed-item" : "baseLozenge"}>
-      <PostHeader orAuth={orAuth} user={user} fi={fi} post={post} />
+    <div id="AudioPost" className="contentFlex" >
+
       <h3>{post.title}</h3>
         <audio  controls src={source}  />
 
         <p>{post.body}</p>
-          <PostFooter post={post} fi={fi} />
     </div>
   )
 }
