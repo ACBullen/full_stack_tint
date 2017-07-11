26287864f3342840943d284939d37e41f078802b
Alexander Bullen <acbullen1990@gmail.com>
2017-07-11 11:51:15 -0700

    Refactor play/pause/mute buttons to be saved on object for further reference. Stlye buttons when audio is playing/paused/muted

--- frontend/components/post_bases/audio_post.jsx
+++ frontend/components/post_bases/audio_post.jsx
@@ -18,12 +18,12 @@ class AudioPost extends React.Component {
       this.props.post.media_link ? source = this.props.post.media_link : source = this.props.post.link_url;
       let audioLoc = document.getElementById(`Audio${this.props.post.id}`);
       audioLoc.innerHTML = '';
-      let audioPlay = document.createElement('audio')
-      audioPlay.id       = `audio-player${this.props.post.id}`;
-      audioPlay.volume   = "0.5"
-      audioPlay.src      = `${source}`;
-      audioPlay.type     = 'audio/mpeg';
-      audioLoc.appendChild(audioPlay);
+      this.audioPlay = document.createElement('audio')
+      this.audioPlay.id       = `audio-player${this.props.post.id}`;
+      this.audioPlay.volume   = "0.5"
+      this.audioPlay.src      = `${source}`;
+      this.audioPlay.type     = 'audio/mpeg';
+      audioLoc.appendChild(this.audioPlay);
       audioLoc.innerHTML +=(
         `<i class="fa fa-play-circle" id="player${this.props.post.id}" aria-hidden="true"></i>
          <i class="fa fa-pause-circle" id="pauser${this.props.post.id}" aria-hidden="true"></i>
@@ -31,12 +31,14 @@ class AudioPost extends React.Component {
          <i class="fa fa-volume-up" id="up${this.props.post.id}" aria-hidden="true"></i>
          <i class="fa fa-volume-off" id="mute${this.props.post.id}"aria-hidden="true"></i>
          `);
-      document.getElementById(`pauser${this.props.post.id}`).addEventListener('click', this.pauseAudio.bind(this));
-      document.getElementById(`player${this.props.post.id}`).addEventListener('click', this.playAudio.bind(this));
+      this.pause = document.getElementById(`pauser${this.props.post.id}`)
+      this.pause.addEventListener('click', this.pauseAudio.bind(this));
+      this.play = document.getElementById(`player${this.props.post.id}`)
+      this.play.addEventListener('click', this.playAudio.bind(this));
       document.getElementById(`up${this.props.post.id}`).addEventListener('click', this.volUp.bind(this));
       document.getElementById(`down${this.props.post.id}`).addEventListener('click', this.volDown.bind(this));
-      document.getElementById(`mute${this.props.post.id}`).addEventListener('click', this.toggleMute.bind(this));
-      console.log(audioPlay.volume);
+      this.mute = document.getElementById(`mute${this.props.post.id}`)
+      this.mute.addEventListener('click', this.toggleMute.bind(this));
       this.audioExposed = true;
     }
     // document.getElementById(`audio-player${this.props.post.id}`).play();
@@ -44,44 +46,51 @@ class AudioPost extends React.Component {
 
   playAudio(){
     // e.preventDefault();
-    let audio = document.getElementById(`audio-player${this.props.post.id}`)
+    let audio = this.audioPlay;
 
     if (audio.paused){
       audio.play();
+      this.play.style.color = "black";
+      this.pause.style.color = "#f9fcff";
     }
   }
 
 
   pauseAudio(){
-    let audio = document.getElementById(`audio-player${this.props.post.id}`)
+    let audio = this.audioPlay;
     if (!audio.paused){
       audio.pause();
+      this.pause.style.color = "black";
+      this.play.style.color = "#f9fcff";
     }
   }
   volUp(){
-    let audio = document.getElementById(`audio-player${this.props.post.id}`)
-    if (!this.muted && audio.volume < 1.0){
+    let audio = this.audioPlay;
+    if (!this.muted){
+      if (audio.volume > 0.9){
+        this.audio.volume = 0.9;
+      }
       audio.volume += .1;
     }
   }
 
   volDown(){
-    let audio = document.getElementById(`audio-player${this.props.post.id}`)
+    let audio = this.audioPlay;
     if (!this.muted && audio.volume > 0.0){
       audio.volume -= .1;
     }
   }
 
   toggleMute(){
-    let audio = document.getElementById(`audio-player${this.props.post.id}`)
-    let muteButton = document.getElementById(`mute${this.props.post.id}`)
+    let audio = this.audioPlay;
+
     if (!this.muted){
-      muteButton.style.color = "black";
+      this.mute.style.color = "black";
       this.baseVol = audio.volume;
       audio.volume = 0.0;
       this.muted = true;
     } else {
-      muteButton.style.color = "#f9fcff";
+      this.mute.style.color = "#f9fcff";
       audio.volume = this.baseVol;
       this.muted = false;
     }
@@ -95,7 +104,7 @@ class AudioPost extends React.Component {
 
         <h3>{this.props.post.title}</h3>
           <div onClick={this.exposeAudio.bind(this)} className="audioDiv" id={`Audio${this.props.post.id}`}>
-            <i className="fa fa-play-circle" aria-hidden="true"></i>
+            <i className="fa fa-file-audio-o" aria-hidden="true"></i>
             </div>
 
           <p>{this.props.post.body}</p>
