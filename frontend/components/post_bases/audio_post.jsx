62e4e1f536789ec895dbf79103b3bcf2dd80c941
Alexander Bullen <acbullen1990@gmail.com>
2017-07-10 20:13:48 -0700

    Add volume control and play/pause buttons once audio is exposed

--- frontend/components/post_bases/audio_post.jsx
+++ frontend/components/post_bases/audio_post.jsx
@@ -7,22 +7,53 @@ class AudioPost extends React.Component {
   constructor(props){
     super(props)
     this.audioExposed = false;
+    this.baseVol = 0.5;
   }
 
-  generateAudio(e){
+  exposeAudio(e){
 
     if(!this.audioExposed){
       let source
       this.props.post.media_link ? source = this.props.post.media_link : source = this.props.post.link_url;
       let audioLoc = document.getElementById(`Audio${this.props.post.id}`);
-      audioLoc.innerHTML = ``;
+      audioLoc.innerHTML = '';
       let audioPlay = document.createElement('audio')
-      audioPlay.id       = 'audio-player';
-      audioPlay.controls = 'controls';
+      audioPlay.id       = `audio-player${this.props.post.id}`;
+      audioPlay.volume   = "0.5"
       audioPlay.src      = `${source}`;
       audioPlay.type     = 'audio/mpeg';
-      audioLoc.appendChild(audioPlay)
+      audioLoc.appendChild(audioPlay);
+      audioLoc.innerHTML +=(`<i class="fa fa-play-circle" id="player${this.props.post.id}" aria-hidden="true"></i>
+    <i class="fa fa-pause-circle" id="pauser${this.props.post.id}" aria-hidden="true"></i>
+    <i class="fa fa-volume-down" id="down${this.props.post.id}" aria-hidden="true"></i>
+    <i class="fa fa-volume-up" aria-hidden="true"></i>
+    <i class="fa fa-volume-off" aria-hidden="true"></i>
+    `);
+      document.getElementById(`pauser${this.props.post.id}`).addEventListener('click', this.pauseAudio.bind(this));
+      document.getElementById(`player${this.props.post.id}`).addEventListener('click', this.playAudio.bind(this));
+      document.getElementById(`up${this.props.post.id}`).addEventListener('click', this.volUp.bind(this));
+      document.getElementById(`down${this.props.post.id}`).addEventListener('click', this.volDown.bind(this));
+      console.log(audioPlay.volume);
       this.audioExposed = true;
+    }
+    // document.getElementById(`audio-player${this.props.post.id}`).play();
+  }
+  playAudio(){
+    // e.preventDefault();
+    let audio = document.getElementById(`audio-player${this.props.post.id}`)
+
+    if (audio.paused){
+      audio.play();
+    }
+  }
+
+
+  pauseAudio(){
+    // e.preventDefault();
+    let audio = document.getElementById(`audio-player${this.props.post.id}`)
+
+    if (!audio.paused){
+      audio.pause();
     }
   }
 
@@ -33,7 +64,7 @@ class AudioPost extends React.Component {
       <div id="AudioPost" className="contentFlex" >
 
         <h3>{this.props.post.title}</h3>
-          <div onClick={this.generateAudio.bind(this)} className="audioDiv" id={`Audio${this.props.post.id}`}>
+          <div onClick={this.exposeAudio.bind(this)} className="audioDiv" id={`Audio${this.props.post.id}`}>
             <i className="fa fa-play-circle" aria-hidden="true"></i>
             </div>
 
